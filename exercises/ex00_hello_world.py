e97a752f29205140eb513a51cf2027b09774d349
checc123 <checc@unc.edu>
2025-04-09 14:43:22 -0400

    change

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -3,10 +3,10 @@
 __author__ = "730549548"
 
 
-def greet(name: str) -> str:
+def greet(name_string: str) -> str:
     """A welcoming first function definition."""
-    return "Hello," + name + "!"
+    return "Hello," + name_string + "!"
 
 
 if __name__ == "__main__":
-    print(greet(name=input("What is your name?")))
+    print(greet(name_string=input("What is your name?")))
